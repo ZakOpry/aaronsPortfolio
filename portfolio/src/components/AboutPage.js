d3e8268fedf343ad39a1cad40a5cdc2fe0f61e52
ZakOpry <88099668+ZakOpry@users.noreply.github.com>
2021-12-20 13:19:20 -0500

    Added Body Content to 'About Page'

--- portfolio/src/components/AboutPage.js
+++ portfolio/src/components/AboutPage.js
@@ -1,11 +1,13 @@
 import React from "react";
 import AboutPagePic from "./AboutPagePic";
 import "../styles/AboutPageStyles.css";
+import AboutPageBody from "./AboutPageBody";
 
 export default function AboutPage() {
   return (
     <div className="entireAboutPageDiv">
       <AboutPagePic />
+      <AboutPageBody />
     </div>
   );
 }
---
+++ portfolio/src/components/AboutPageBody.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "../styles/AboutPageStyles.css";
+
+export default function AboutPageBody() {
+  return (
+    <div className="bodyDivContainer">
+      <div className="AboutPageBodyDiv">
+        <div className="aboutTitle">
+          <h1>Tug Films.</h1>
+        </div>
+        <div className="bioDiv">
+          <p>
+            Hi there. I'm Tug, and I kind of have a thing for story telling, and
+            would love to tell yours. The day my wife and I got married, I was a
+            nervous WRECK. It was a long day to say the least. Between dealing
+            with my emotions, finally saying I do, and celebrating with our
+            friends and family, I was exhausted. And before I knew it, it was
+            over. Just like that. One of the happiest days of my life; as slow
+            as it came was as quick as it went. With all the chaos and
+            excitement of the day, I didn't quite remember all the details like
+            I had hoped, and I was pretty sad about it to be honest. That
+            feeling drove me to create what I call, Tug Films. I want you to
+            have the opportunity to relive all of the special moments from your
+            big day for years to come, and be able to share your perfectly
+            curated story with the people that mean the most to you. I'm a very
+            laid back person, easy to talk to, and I really look forward to
+            hearing about your story.{" "}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
