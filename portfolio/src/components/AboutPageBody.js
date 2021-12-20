2a5c9ec61823ef5122de6f6bc30ae02237dc5917
ZakOpry <88099668+ZakOpry@users.noreply.github.com>
2021-12-20 16:12:51 -0500

    Styled the about page to add more mobile responsiveness

--- portfolio/src/components/AboutPageBody.js
+++ portfolio/src/components/AboutPageBody.js
@@ -10,21 +10,20 @@ export default function AboutPageBody() {
         </div>
         <div className="bioDiv">
           <p>
-            Hi there. I'm Tug, and I kind of have a thing for story telling, and
-            would love to tell yours. The day my wife and I got married, I was a
-            nervous WRECK. It was a long day to say the least. Between dealing
+            Hi there! I'm Tug, and I kind of have a thing for story telling and
+            would love to tell yours. The day my wife and I got married I was a
+            nervous wreck it was a long day to say the least! Between dealing
             with my emotions, finally saying I do, and celebrating with our
-            friends and family, I was exhausted. And before I knew it, it was
-            over. Just like that. One of the happiest days of my life; as slow
-            as it came was as quick as it went. With all the chaos and
-            excitement of the day, I didn't quite remember all the details like
-            I had hoped, and I was pretty sad about it to be honest. That
-            feeling drove me to create what I call, Tug Films. I want you to
-            have the opportunity to relive all of the special moments from your
-            big day for years to come, and be able to share your perfectly
-            curated story with the people that mean the most to you. I'm a very
-            laid back person, easy to talk to, and I really look forward to
-            hearing about your story.{" "}
+            friends and family, I was exhausted. Before I knew it, the day was
+            over. One of the happiest days of my life; as slow as it came was as
+            quick as it went. With all the chaos and excitement of the day I
+            didn't quite remember all the details like I had hoped and to be
+            honest I was pretty sad about it. That feeling drove me to create
+            what I call "Tug Films". I want you to have the opportunity to
+            relive all of the special moments from your big day for years to
+            come and be able to share your perfectly curated story with the
+            people that mean the most to you. I'm a very laid back person, easy
+            to talk to, and I really look forward to hearing about your story!{" "}
           </p>
         </div>
       </div>
